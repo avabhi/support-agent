8042df356cac2feb3093eeab6894a1863068f683
Abhinav Vishwakarma <abhinav@Abhinavs-MacBook-Air.local>
2025-06-16 22:37:14 +0530

    added github workflows

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/support-agent/",
+  server: {
+    open: true,
+    port: 3000,
+  },
 });
